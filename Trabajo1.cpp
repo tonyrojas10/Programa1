3f4db61e88f93159361e8808d0bc6ab4bfe28d83
tonyrojas10 <tonyestuardor@gmail.com>
2016-02-19 17:35:14 -0600

    Added files via upload

---
+++ Trabajo1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Trabajo1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hola Mundo!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
